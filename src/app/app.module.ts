0665055b19688c015714c26eb559fbf6405a4a54
Janayna Moura <fernandesmjanayna@gmail.com>
2020-04-30 17:04:22 -0300

    aula 9.14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FuncionarioCardComponent
   ],
   imports: [
     BrowserModule,
