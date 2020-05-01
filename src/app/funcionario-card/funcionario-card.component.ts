cab6400245f83dded323dfcb28ba2d50f7fa10ce
Janayna <fernandesmjanayna@gmail.com>
2020-04-30 23:18:10 -0300

    aula 9.18

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -9,4 +9,17 @@ export class FuncionarioCardComponent {
 
   @Input() funcionario: any;
 
+  getEstilosCartao(){
+    return {
+      'border-width.px': this.funcionario.id,
+      backgroundColor: this.funcionario.id % 2 === 0 ? 'lightblue' : 'lightgreen'
+    };
+  }
+
+  // getListaClassesCss() {
+  //   return ['badge', 'badge-default'];
+  // }
+  isAdmin() {
+    return this.funcionario.nome.startsWith('T');
+  }
 }
