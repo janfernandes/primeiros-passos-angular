63b87aee2b767f8bab38ca10b02832f742ee129d
Janayna Moura <fernandesmjanayna@gmail.com>
2020-05-08 10:26:23 -0300

    aula 11.3

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,13 +1,32 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
 })
 export class CampoColoridoDirective {
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
-    console.log(this.elementRef.nativeElement);
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+  @HostBinding('style.backgroundColor') corDeFundo: string;
+
+  @HostListener('focus') aoGanharFoco(){
+    this.corDeFundo = 'green';
   }
 
+  @HostListener('blur') aoPerderFoco(){
+    this.corDeFundo = 'transparent';
+  }
+
+
+
+
+  // constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  //   // console.log(this.elementRef.nativeElement);
+  // }
+  //
+  // @HostListener('focus') aoGanharFoco(){
+  //   this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+  // }
+  //
+  // @HostListener('blur') aoPerderFoco(){
+  //   this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  // }
 }
