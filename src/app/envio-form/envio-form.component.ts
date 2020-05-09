664c8d82f03dc696488ab3eca2c14c3219e6799f
Janayna <fernandesmjanayna@gmail.com>
2020-05-09 17:34:42 -0300

    aula 12.13

--- src/app/envio-form/envio-form.component.ts
+++ src/app/envio-form/envio-form.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import {NgForm} from '@angular/forms';
 
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+}
+
 @Component({
   selector: 'app-envio-form',
   templateUrl: './envio-form.component.html',
   styleUrls: ['./envio-form.component.css']
 })
 export class EnvioFormComponent{
-  profissao = 'Outra';
+  cliente = new Cliente();
+  prof = 'Outra';
   profissoes = ['Programador', 'Empresário', 'Outra'];
 
   salvar(form: NgForm) {
+    // this.cliente.nome = form.value.nome;
+    // this.cliente.email = form.value.email;
+    // this.cliente.profissao = form.value.profissao;
     console.log(form);
+    form.reset({profissao: ''});
   }
 
 }
